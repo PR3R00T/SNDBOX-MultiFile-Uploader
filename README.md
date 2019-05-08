66520e45218be437a51eca9d309410c790e1588a
PR3R00T <PR3R00T@protonmail.com>
2019-05-08 20:39:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# SNDBOX-MultiFile-Uploader
+Upload multiple samples to the SNDBOX Malware analysis API. 
+
+This uploader was created to upload samples captured by the Dionaea and Adbhoney pots installed with the TPot Framework (https://github.com/dtag-dev-sec/tpotce) This Framework deploys a range of honeypots targeted to capture and analyse traffic. The two honeypots, Dionaea and Adbhoney also capture Malware samples.
+
+Combining this script with a cron job, all samples for each day will be checked against SNDBOX (https://app.sndbox.com) and will upload the samples if required.
+
+To operate the Uploader, follow the instructions:
+
+    Clone this Repo: git clone https://github.com/PR3R00T/SNDBOX-MultiFile-Uploader.git
+
+You will need a Free SNDBOX account (https://app.sndbox.com/register) Once completed navigate to the User Settings -> API key and copy the key. 2.Place the key into the variable: apikey in the SNDBOXupload.py script. (nano SNDBOXupload.py)
+
+3.Install the Requirements.txt: pip3 install -r Requirements.txt
+
+4.Give the file execute permissions: chmod +x SNDBOXupload.py
+
+5.run the script: ./SNDBOXupload.py
+
+Log files are created within: /data/ folder if any issues occur, check the SNDBOXuploaderror log.
+
+If you would like to automate this script add a cron job:
+
+    launch crontab with: crontab -e
+
+    append to the end (change the times if required):
+
+m h dom mon dow command
+
+20 2 * * * /full/path/to/SNDBOX-MultiFile-Uploader/SNDBOXupload.py
---
+++ SNDBOXupload.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import sys
+from sndbox import sndboxapi as SNDBOX
+import os
+import glob
+import time
+from filehash import FileHash
+
+#API KEY Goes here
+apikey = "key123"
+
+#Populating list of Samples
+dionaea_bin_path = "/data/dionaea/binaries/*"
+adbhoney_bin_path = "/data/adbhoney/downloads/*"
+dionaea_samples = glob.glob(dionaea_bin_path)
+adbhoney_samples = glob.glob(adbhoney_bin_path)
+sample_list = dionaea_samples + adbhoney_samples
+
+md5hasher = FileHash('md5')
+timestr = time.strftime("%Y%m%d")
+
+#Log File paths
+output_file = "/data/SNDBOXupload"+timestr+".txt"
+patherror =  "/data/SNDBOXuploaderror" + timestr + ".txt"
+
+# iterate each file
+for filename in sample_list:
+    print("[*] Found file: " + filename)
+    #Get hash of file
+    hash = md5hasher.hash_file(filename)
+    try:
+        file_check = SNDBOX.search(apikey=apikey,md5hash=hash)
+    except Exception as e:
+        f= open(patherror,"a+")
+        f.write(file + " Failed to check: " + str(e) + "\n")
+        f.close()
+        print("Failed to check: "+ hash)
+    if file_check.status_code == 200:
+        print("[*] Sample already uploaded, ignoring")
+    elif file_check.status_code == 404:
+        print("[*] New sample found! - " + hash)
+        try:
+            upload_response = SNDBOX.submit(apikey=apikey,file=filename,email=False)
+        except Exception as e:
+            f= open(patherror,"a+")
+            f.write(file + " Failed to upload: " + str(e) + "\n")
+            f.close()
+            print("Failed to upload: " + hash)
+        if upload_response.status_code == 201:
+            print("[*] Sample uploaded to SNDBOX! - " + hash)
+            f= open(output_file,"a+")
+            f.write("Hash Uploaded: - " + hash + "\n")
+            f.close()
